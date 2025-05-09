4625ebedaefb579edaa27891af8164022f66cb99
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-05-09 13:26:17 +0900

    (2025.5.9) Project Initialization

---
+++ Game/Assets/Class12th (Ray Cast)/Scripts/Mouse.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] Ray ray;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] RaycastHit rayCastHit;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out rayCastHit, Mathf.Infinity, layerMask))
+            {
+                GameObject prefab = rayCastHit.collider.gameObject;
+
+                if(prefab.transform.GetChild(0).gameObject.activeSelf)
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(false);
+                }
+                else
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
